fd600eeee9da762dc6adc5b64fb7ac7c8dd7a5a1
Franz See <franz@see.net.ph>
2019-04-16 00:54:46 +0800

    Replaced swagger docs with slate docs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
-var SwaggerUi = require('swagger-tools/middleware/swagger-ui');
 
-var app = require('express')();
+const express = require('express')
+var app = express();
 module.exports = app; // for testing
 
 var config = {
@@ -13,11 +13,15 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
-  app.use(SwaggerUi(swaggerExpress.runner.swagger));
-
   // install middleware
   swaggerExpress.register(app);
 
+  const router = express.Router();
+  router.get('/', (req, res) => {
+    res.redirect('/docs');
+  })
+  app.use('/', express.static('out/docs'))
+  
   var port = process.env.PORT || 3000;
   console.log('Starting up 0.0.0.0:' + port)
   app.listen(port);
