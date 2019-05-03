821d711c1a30d3d58a99e5eb6476f3af2e5dff11
Franz See <franz@see.net.ph>
2019-05-04 05:06:57 +0800

    Updated docu generation based on the docusaurus ccxt-rest-website

--- scripts/generate-docs.js
+++ scripts/generate-docs.js
@@ -3,7 +3,8 @@
 const fs = require('fs');
 const execSync = require('child_process').execSync;
 
-const widdershins_output_file = './docs/source/_tmp.html.md'
+const redoc_output_file = './docs/website/static/api.html'
+const ccxt_rest_website_output_dir = 'docs/website/build/ccxt-rest'
 
 // https://stackoverflow.com/questions/18052762/remove-directory-which-is-not-empty#answer-32197381
 var _deleteFolderRecursive = function(path) {
@@ -30,50 +31,30 @@ var _deleteFolderRecursive = function(path) {
 }
 
 function ensure_submodule_exists() {
-    if (!fs.existsSync('./docs/Rakefile')) {
+    if (!fs.existsSync('./docs/Dockerfile')) {
         run_command('git submodule init docs')
         run_command('git submodule update docs')
     }
 }
 
-function widdershins() {
+function redoc() {
     var command = [
-        "./node_modules/.bin/widdershins", 
-        "--search", "true", 
-        "--language_tabs", "'shell:Shell'", "'go:Go'", "'java:Java'", "'javascript:Javascript'", "'python:Python'", "'ruby:Ruby'", 
-        "--summary", "api/swagger/swagger.yaml", 
-        "-o", widdershins_output_file
+        "./node_modules/.bin/redoc-cli", 
+        "bundle", "api/swagger/swagger.yaml", 
+        "--output", redoc_output_file, 
+        "--title='CCXT-REST API Documentation'", 
+        "--template=./docs/templates/template.hbs"
     ]
     run_command(command.join(' '));
 }
 
-function split_widdershins_output() {
-    console.info('[START] split_widdershins_output()...')
-    const data = fs.readFileSync(widdershins_output_file, 'utf8')
-
-    const delimiter = '<h1 id="ccxt-rest-authentication-api">Authentication API</h1>'
-    segments = data.split(delimiter);
-
-    fs.writeFileSync("./docs/source/00_generated_header.html.md", [segments[0], '\n'].join(''));
-    fs.writeFileSync("./docs/source/50_generated_api.html.md", ['\n', delimiter, segments[1]].join(''));
-
-    fs.unlinkSync(widdershins_output_file)
-
-    console.info('[END] split_widdershins_output()...')
-}
-
 function docs_build() {
-    run_command('rake build', {cwd:'./docs'});
-    if (!fs.existsSync('./docs/build')) {
-        console.error('Should have been able to find ./docs/build but did not')
+    run_command('npm install', {cwd:'./docs/website'});
+    run_command('npm run build', {cwd:'./docs/website'});
+    if (!fs.existsSync(ccxt_rest_website_output_dir)) {
+        console.error(`Should have been able to find ${ccxt_rest_website_output_dir} but did not`)
         process.exit(1)
     }
-}
-
-function delete_out_docs() {
-    console.info('[START] delete_out_docs()...')
-    _deleteFolderRecursive('./out/docs')
-    console.info('[END] delete_out_docs()...')
 }
 
 function move_docs_build_to_out_docs() {
@@ -87,17 +68,17 @@ function move_docs_build_to_out_docs() {
     }
     try {
         // this code fails inside docker
-        fs.renameSync('docs/build', 'out/docs')
+        fs.renameSync(ccxt_rest_website_output_dir, 'out/docs')
     } catch (err) {
         // so we fallback to something we know will work inside node:10.4.0-alpine
-        run_command('mv docs/build out/docs')
+        run_command(`mv ${ccxt_rest_website_output_dir} out/docs`)
     }
     console.info('[END] move_docs_build_to_out_docs()...')
 }
 
 ensure_submodule_exists();
-widdershins();
-split_widdershins_output();
+_deleteFolderRecursive('./docs/website/build')
+_deleteFolderRecursive('./out/docs')
+redoc();
 docs_build();
-delete_out_docs();
 move_docs_build_to_out_docs();
